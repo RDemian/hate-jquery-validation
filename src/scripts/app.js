83d272219e1b77a064ca9daa84f7c47e9938d721
Matvey Phoenix <matvey.phoen1x@yandex.ru>
2018-10-02 17:23:42 +0500

    Успешно подключил jquery validation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,35 @@
-$('.form').on('submit', (e) => {
-  e.preventDefault();
+$(document).ready(function () {
+    const $form = $('.form');
 
-  const $form = $('.form');
-})
+    $form.on('submit', (e) => {
+        e.preventDefault();
+    })
+
+    $form.validate({
+
+        submitHandler: function (form) {
+            console.log('Форма успешно отправлена!');
+        },
+
+        rules: {
+            firstName: {
+                required: true,
+                minlength: 2,
+            },
+            secondName: {
+                required: true,
+                minlength: 2,
+            },
+        },
+        messages: {
+            firstName: {
+                required: "Укажите своё имя",
+                minlength: "Имя должно быть длиннее 2 символов",
+            },
+            secondName: {
+                required: "Укажите свою фамилию",
+                minlength: "Фамилия должна быть длиннее 2 символов",
+            },
+        }
+    });
+});
